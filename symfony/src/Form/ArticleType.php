38d81a1f64befd0b77db79ba8c9f4eec6bbb8e70
rogercrdaw <a19rogcalrul@inspedralbes.cat>
2020-05-11 16:01:22 +0100

    ajusteant l'entitat article i el seu corresponent formulari

--- symfony/src/Form/ArticleType.php
+++ symfony/src/Form/ArticleType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,7 +20,11 @@ class ArticleType extends AbstractType
             ->add('contingut')
             ->add('tag_meta')
             ->add('tag_web')
-            ->add('tema')
+            ->add('tema', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+                'mapped' => false,
+            ])
         ;
     }
 
