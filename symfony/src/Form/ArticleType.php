9010313cb10602c7ac6e88d1c0cb026d3aebd40a
rogercrdaw <a19rogcalrul@inspedralbes.cat>
2020-05-13 10:52:58 +0100

    form editar categories, mes frontend....

--- symfony/src/Form/ArticleType.php
+++ symfony/src/Form/ArticleType.php
@@ -5,12 +5,12 @@ namespace App\Form;
 use App\Entity\Article;
 use App\Entity\Categoria;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Lenght;
 
 
