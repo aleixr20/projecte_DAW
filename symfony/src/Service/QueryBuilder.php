f6b8cb241238e7634d13e188b08d1925a6badc78
Aleix <a14alerevgau@inspedralbes.cat>
2020-05-21 14:01:50 +0200

    Eliminat SET GLOBAL event_scheduler

--- symfony/src/Service/QueryBuilder.php
+++ symfony/src/Service/QueryBuilder.php
@@ -23,19 +23,16 @@ class QueryBuilder
         $shortToken = substr($user->getToken(), 0, 10);
         $entityManager = $this->entityManager;
 
-        $query1 = 'SET GLOBAL event_scheduler=ON;';
-        $query2 = 'CREATE EVENT '.'autodestroy_token'.$shortToken.' 
+        $query = 'CREATE EVENT '.'autodestroy_token'.$shortToken.' 
                         ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 HOUR
                         DO UPDATE user
                         SET token = NULL
                         WHERE id = :id;';
-        
-        $statement1 = $entityManager->getConnection()->prepare($query1);
-        $statement2 = $entityManager->getConnection()->prepare($query2);
 
-        $statement2->bindValue('id', $user->getId());
+        $statement = $entityManager->getConnection()->prepare($query);
 
-        $statement1->execute();
-        $statement2->execute();
+        $statement->bindValue('id', $user->getId());
+
+        $statement->execute();
     }
 }
