aad0f405c93416572938160b16aed81095e1f7e6
rogercrdaw <a19rogcalrul@inspedralbes.cat>
2020-05-23 14:18:10 +0100

    he afegit alguns usuaris a fixtures i algun color random a categories per veure el comportament i gestionar visibilitat segons roles

--- symfony/src/DataFixtures/ArticlesFixtures.php
+++ symfony/src/DataFixtures/ArticlesFixtures.php
@@ -32,13 +32,59 @@ class ArticlesFixtures extends Fixture
             ->setNomUsuari('aleixMaki')->setDataRegistre(new DateTime())
             ->setPassword($this->passwordEncoder->encodePassword($admin, "admin"))
             ->setImatge('perfil_random.png')
+            ->setDescripcio('
+<p>Mi eget mauris pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et netus et malesuada fames ac.Vulputate dignissim suspendisse in est ante in nibh. Enim ut sem viverra aliquet eget sit amet tellus cras. Mattis molestie a iaculis at erat pellentesque adipiscing. Sed risus ultricies tristique nulla.</p>
+<p>Tortor condimentum lacinia quis vel eros donec ac odio. Varius quam quisque id diam vel quam elementum pulvinar etiam. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Id semper risus in hendrerit. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa. Fermentum posuere urna nec tincidunt praesent.</p>
+            ')
             ->setGithub('aleixr20')
             ->setLinkedin('aleixLink20');
-
         $roles = ["ROLE_ADMIN"];
         $admin->setRoles($roles);
-
         $manager->persist($admin);
+
+        $user1 = new User();
+        $user1->setEmail('user1@admin.com')
+            ->setNom('User')->setCognom('Uname')
+            ->setNomUsuari('usuario')->setDataRegistre(new DateTime())
+            ->setPassword($this->passwordEncoder->encodePassword($user1, "user1"))
+            ->setImatge('62-5ec4629f12c91.jpeg')
+            ->setDescripcio('
+<p>Mi eget mauris pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et ltricies tristique nulla.</p>
+<p>Tortor condimentum lacinia quis vel eros donec ac odio. Varius quam quisque id diam vel quam elementum pulvinar etiam. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Id semper risus in hendrerit. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa. Fermentum posuere urna nec tincidunt praesent.</p>
+            ')
+            ->setGithub('yoquese')
+            ->setLinkedin('miLinkedin');
+        $roles = ["ROLE_VALIDATED"];
+        $user1->setRoles($roles);
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('user2@admin.com')
+            ->setNom('Paco')->setCognom('Malote')
+            ->setNomUsuari('bigHacker')->setDataRegistre(new DateTime())
+            ->setPassword($this->passwordEncoder->encodePassword($user2, "user2"))
+            ->setDescripcio('
+<p>Pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et netus et malesuada fames ac.Vulputate dignissim suspendisse in est ante in nibh. Enim ut sem viverra aliquet eget sit amet tellus cras. Mattis molestie a iaculis at erat pellentesque adipiscing. Sed risus ultricies tristique nulla.</p>
+<p>Tortor condimentum lacam quisque id diam vel quam elementum pulvinar etiam. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Id semper risus in hendrerit. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa. Fermentum posuere urna nec tincidunt praesent.</p>
+            ')
+            ->setGithub('hacker')
+            ->setLinkedin('random');
+
+        $roles = ["ROLE_VALIDATED"];
+        $user2->setRoles($roles);
+        $manager->persist($user2);
+
+
+        $user3 = new User();
+        $user3->setEmail('user3@admin.com')
+            ->setNom('Anonim')->setCognom('No Verified')
+            ->setNomUsuari('quePalo')->setDataRegistre(new DateTime())
+            ->setPassword($this->passwordEncoder->encodePassword($user3, "user3"));
+
+        $roles = ["ROLE_USER"];
+        $user3->setRoles($roles);
+        $manager->persist($user3);
+
 
         //Creem algunes categories predefinides per anar provant
         $categoria_html = new Categoria();
@@ -50,24 +96,29 @@ class ArticlesFixtures extends Fixture
         $categoria_css = new Categoria();
         $categoria_css->setNom('CSS')
             ->setTipus('frontend')
+            ->setColor('royalblue')
             ->setLogo('css-3-5ebbbde6704a2.svg');
+
         $manager->persist($categoria_css);
 
         $categoria_bootstrap = new Categoria();
         $categoria_bootstrap->setNom('Bootstrap')
             ->setTipus('frontend')
+            ->setColor('purple')
             ->setLogo('bootstrap_logo.jpg');
         $manager->persist($categoria_bootstrap);
 
         $categoria_js = new Categoria();
         $categoria_js->setNom('JS')
             ->setTipus('frontend')
+            ->setColor('gold')
             ->setLogo('js-5ebfc77478271.png');
         $manager->persist($categoria_js);
 
         $categoria_vue = new Categoria();
         $categoria_vue->setNom('VUE')
             ->setTipus('frontend')
+            ->setColor('mediumseagreen')
             ->setLogo('vue_logo.png');
         $manager->persist($categoria_vue);
 
@@ -246,7 +297,7 @@ $result = $serializer-&gt;normalize($level1, null, [
         $manager->persist($article_apa1);
 
         //ARTICLE 4
-        $titol_nginx1 = "Configuració Nginx";
+        $titol_nginx1 = "Algo de JS";
 
         $article_nginx1 = new Article();
         $article_nginx1->setTitol($titol_nginx1)
@@ -254,7 +305,7 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_nginx1)))
             ->setUser($admin)
-            ->setCategoria($categoria_nginx)
+            ->setCategoria($categoria_js)
             ->setMetaTag('servidor,nginx,localhost')
             ->setContingut('
 <p>Metus dictum at tempor commodo. Sapien et ligula ullamcorper malesuada proin libero nunc. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Risus nec feugiat in fermentum. Pulvinar sapien et ligula ullamcorper malesuada proin. Mi eget mauris pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et netus et malesuada fames ac.</p>
@@ -278,7 +329,7 @@ $result = $serializer-&gt;normalize($level1, null, [
         $manager->persist($article_nginx1);
 
         //ARTICLE 5
-        $titol_sql1 = "Sentencies SQL";
+        $titol_sql1 = "funcions de geolocalitzacio amb JS";
 
         $article_sql1 = new Article();
         $article_sql1->setTitol($titol_sql1)
@@ -286,7 +337,7 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_sql1)))
             ->setUser($admin)
-            ->setCategoria($categoria_sql)
+            ->setCategoria($categoria_js)
             ->setMetaTag('bbdd,sql,')
             ->setContingut('
 <p>Metus dictum at tempor commodo. Sapien et ligula ullamcorper malesuada proin libero nunc. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Risus nec feugiat in fermentum. Pulvinar sapien et ligula ullamcorper malesuada proin. Mi eget mauris pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et netus et malesuada fames ac.</p>
@@ -317,7 +368,7 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setSubtitol("Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa.")
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_mongo)))
-            ->setUser($admin)
+            ->setUser($user1)
             ->setCategoria($categoria_mongo)
             ->setMetaTag('servidor,nginx,localhost')
             ->setContingut('
@@ -338,7 +389,7 @@ $result = $serializer-&gt;normalize($level1, null, [
 
 <p>Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Tempor commodo ullamcorper a lacus vestibulum sed arcu. Sit amet volutpat consequat mauris nunc congue. Dis parturient montes nascetur ridiculus mus mauris vitae. Magna fermentum iaculis eu non diam phasellus vestibulum.</p>
             ')
-            ->setVisible(true);
+            ->setVisible(false);
         $manager->persist($article_mongo1);
 
         //ARTICLE 7
@@ -349,7 +400,7 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setSubtitol("Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa.")
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_symfony1)))
-            ->setUser($admin)
+            ->setUser($user2)
             ->setCategoria($categoria_symfony)
             ->setMetaTag('servidor,nginx,localhost')
             ->setContingut('
@@ -376,7 +427,7 @@ $result = $serializer-&gt;normalize($level1, null, [
 </code></pre> 
 
             ')
-            ->setVisible(true);
+            ->setVisible(false);
         $manager->persist($article_symfony1);
 
         //ARTICLE 8
@@ -387,7 +438,7 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setSubtitol("Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa.")
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_symfony2)))
-            ->setUser($admin)
+            ->setUser($user2)
             ->setCategoria($categoria_symfony)
             ->setMetaTag('servidor,nginx,localhost')
             ->setContingut('
@@ -419,7 +470,7 @@ $result = $serializer-&gt;normalize($level1, null, [
         $manager->persist($article_symfony2);
 
         //ARTICLE 9
-        $titol_java1 = "Crear una classe amb Java";
+        $titol_java1 = "VUE contra API";
 
         $article_java1 = new Article();
         $article_java1->setTitol($titol_java1)
@@ -427,7 +478,7 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_java1)))
             ->setUser($admin)
-            ->setCategoria($categoria_java)
+            ->setCategoria($categoria_vue)
             ->setMetaTag('servidor,nginx,localhost')
             ->setContingut('<
 p>Metus dictum at tempor commodo. Sapien et ligula ullamcorper malesuada proin libero nunc. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Risus nec feugiat in fermentum. Pulvinar sapien et ligula ullamcorper malesuada proin. Mi eget mauris pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et netus et malesuada fames ac.</p>
@@ -491,14 +542,14 @@ $result = $serializer-&gt;normalize($level1, null, [
         $manager->persist($article_java2);
 
         //ARTICLE 11
-        $titol_html1 = "Getters i setters(Java)";
+        $titol_html1 = "Algo de HTML";
 
         $article_htm1 = new Article();
         $article_htm1->setTitol($titol_html1)
             ->setSubtitol("Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa.")
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_html1)))
-            ->setUser($admin)
+            ->setUser($user1)
             ->setCategoria($categoria_html)
             ->setMetaTag('servidor,nginx,localhost')
             ->setContingut('
@@ -535,7 +586,7 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setSubtitol("Exemple bàsic de la estructura HTML")
             ->setDataPublicacio(new DateTime())
             ->setSlug(str_replace(" ", "-", strtolower($titol_html2)))
-            ->setUser($admin)
+            ->setUser($user1)
             ->setCategoria($categoria_html)
             ->setMetaTag('html,disseny, estructures')
             ->setContingut('
@@ -579,43 +630,8 @@ $result = $serializer-&gt;normalize($level1, null, [
             ->setVisible(true);
         $manager->persist($article_html2);
 
-        //ARTICLE 13
-        $titol_html1 = "Getters i setters(Java)";
 
-        $article_htm1 = new Article();
-        $article_htm1->setTitol($titol_html1)
-            ->setSubtitol("Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa.")
-            ->setDataPublicacio(new DateTime())
-            ->setSlug(str_replace(" ", "-", strtolower($titol_html1)))
-            ->setUser($admin)
-            ->setCategoria($categoria_html)
-            ->setMetaTag('servidor,nginx,localhost')
-            ->setContingut('
-<p>Metus dictum at tempor commodo. Sapien et ligula ullamcorper malesuada proin libero nunc. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Risus nec feugiat in fermentum. Pulvinar sapien et ligula ullamcorper malesuada proin. Mi eget mauris pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et netus et malesuada fames ac.</p>
-<p>Tortor condimentum lacinia quis vel eros donec ac odio. Varius quam quisque id diam vel quam elementum pulvinar etiam. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Id semper risus in hendrerit. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Urna et pharetra pharetra massa massa. Fermentum posuere urna nec tincidunt praesent.</p>
-<ul>
-    <li>Elementum pulvinar etiam</li>
-    <li>non quam lacus suspendisse faucibus interdum posuere.</li>
-    <li>Urna et pharetra pharetra massa massa.</li>
-    <li>Fermentum posuere urna nec tincidunt praesent.</li>
-</ul>
-<p>Mi eget mauris pharetra et ultrices neque ornare. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Aliquet porttitor lacus luctus accumsan tortor posuere. Morbi tristique senectus et netus et malesuada fames ac.Vulputate dignissim suspendisse in est ante in nibh. Enim ut sem viverra aliquet eget sit amet tellus cras. Mattis molestie a iaculis at erat pellentesque adipiscing. Sed risus ultricies tristique nulla.</p>
-<pre><code class="language-php hljs">
-$result = $serializer-&gt;normalize($level1, null, [
-    AbstractObjectNormalizer::ENABLE_MAX_DEPTH =&gt; true
-]);
-</code></pre> 
 
-<ul>
-    <li>Elementum pulvinar etiam</li>
-    <li>non quam lacus suspendisse faucibus interdum posuere.</li>
-    <li>Urna et pharetra pharetra massa massa.</li>
-    <li>Fermentum posuere urna nec tincidunt praesent.</li>
-</ul>
-            ')
-            ->setVisible(true);
-        $manager->persist($article_htm1);
-        //hola
 
         $manager->flush();
     }
